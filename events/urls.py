81091171c4cb547ac45d3c09fb16719b1c4062c9
miernik <miernik@cloca.net>
2009-11-11 23:41:35 +0100

    some more clean-up

--- events/urls.py
+++ events/urls.py
@@ -9,16 +9,17 @@ info_dict = {
 }
 
 urlpatterns = patterns('',
-    (r'^edit/(?P<event_id>\d+)/$',        'gridcalendar.events.views.edit'),
-    (r'^edit_astext/(?P<event_id>\d+)/$', 'gridcalendar.events.views.edit_astext'),
-    (r'^view_astext/(?P<event_id>\d+)/$', 'gridcalendar.events.views.view_astext'),
-    (r'^(?P<object_id>\d+)/$',            'django.views.generic.list_detail.object_detail', info_dict),
-    (r'^search/',                         'events.views.search'),
-    (r'^u/(?P<username>\w+)/',            'events.views.search_byuser'),
-    (r'^user/(?P<username>\w+)/',         'events.views.search_byuser'),
-    (r'^my/',                             'events.views.search_thisuser'),
     (r'^simplified_submission/',          'events.views.simplified_submission'),
-    (r'^tags/(?P<tag_name>[^/]+)/$',      'gridcalendar.events.views.events_for_tag'),
+#
+    (r'^edit/(?P<event_id>\d+)/$',        'events.views.edit'),
+    (r'^edit_astext/(?P<event_id>\d+)/$', 'events.views.edit_astext'),
+    (r'^view_astext/(?P<event_id>\d+)/$', 'events.views.view_astext'),
+    (r'^(?P<object_id>\d+)/$',            'django.views.generic.list_detail.object_detail', info_dict),
+#
+    (r'^list/search/',                     'events.views.list_search'),
+    (r'^list/user/(?P<username>\w+)/',     'events.views.list_user'),
+    (r'^list/my/',                         'events.views.list_my'),
+    (r'^list/tags/(?P<tag_name>[^/]+)/$',  'events.views.list_tag'),
 #    (r'^tags/(?P<tag>[^/]+)/$',           'tagging.views.tagged_object_list', {'queryset_or_model': Event}),
     # never reached because ../urls.py also has ^$ :
     (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
