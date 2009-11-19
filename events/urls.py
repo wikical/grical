a666a53bffbabb55051e7af185358bfa95b2c6f6
miernik <miernik@users.noreply.github.com>
2009-11-19 17:17:06 +0100

    Now one can save filters and almost use them

--- events/urls.py
+++ events/urls.py
@@ -9,20 +9,21 @@ info_dict = {
 }
 
 urlpatterns = patterns('',
-    (r'^simplified_submission/',          'events.views.simplified_submission'),
+    (r'^simplified_submission/',                    'events.views.simplified_submission'),
 #
-    (r'^edit/(?P<event_id>\d+)/$',        'events.views.edit'),
-    (r'^edit_astext/(?P<event_id>\d+)/$', 'events.views.edit_astext'),
-    (r'^view_astext/(?P<event_id>\d+)/$', 'events.views.view_astext'),
-    (r'^(?P<object_id>\d+)/$',            'django.views.generic.list_detail.object_detail', info_dict),
+    (r'^edit/(?P<event_id>\d+)/$',                  'events.views.edit'),
+    (r'^edit_astext/(?P<event_id>\d+)/$',           'events.views.edit_astext'),
+    (r'^view_astext/(?P<event_id>\d+)/$',           'events.views.view_astext'),
+    (r'^(?P<object_id>\d+)/$',                      'django.views.generic.list_detail.object_detail', info_dict),
 #
-    (r'^list/search/',                     'events.views.list_search'),
-    (r'^list/search_save/',                'events.views.save_search'),
-    (r'^list/search_save_edit/(?P<savedsearch_id>\d+)/',  'events.views.saved_search_edit'),
+    (r'^list/filter/list/',                         'events.views.filter_list'),
+    (r'^list/filter/save/',                         'events.views.filter_save'),
+    (r'^list/filter/edit/(?P<savedsearch_id>\d+)/', 'events.views.filter_edit'),
 #
-    (r'^list/user/(?P<username>\w+)/',     'events.views.list_user'),
-    (r'^list/my/',                         'events.views.list_my'),
-    (r'^list/tags/(?P<tag>[ \-\w]*)/$' ,  'events.views.list_tag'),
+    (r'^list/user/(?P<username>\w+)/',              'events.views.list_user_events'),
+    (r'^list/my/',                                  'events.views.list_my_events'),
+    (r'^list/tags/(?P<tag>[ \-\w]*)/$' ,            'events.views.list_tag'),
+    (r'^list/search/',                              'events.views.list_search'),
 # never reached because ../urls.py also has ^$ :
-    (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^$',                                         'django.views.generic.list_detail.object_list', info_dict),
 )
