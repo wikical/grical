320d34b2ad88de609565e34212fe02f7868ff1f8
miernik <miernik@users.noreply.github.com>
2009-12-03 02:49:37 +0100

    fixup of messages

--- groups/views.py
+++ groups/views.py
@@ -49,7 +49,7 @@ def list_my_groups(request):
                 {'title': 'list my groups', 'groups': groups},
                 context_instance=RequestContext(request))
 
-def quit_group(request, group_id):
+def quit_group(request, group_id, sure):
     if ((not request.user.is_authenticated()) or (request.user.id is None)):
         return render_to_response('error.html',
                 {'title': 'error', 'message_col1': _("You must be logged in to quit your groups") + "."},
@@ -58,9 +58,18 @@ def quit_group(request, group_id):
         u = User(request.user)
         try:
             g = Group.objects.get(id=group_id, membership__user=u)
-            m = Membership.objects.get(user=request.user, group=g)
-            m.delete()
-            return HttpResponseRedirect('/groups/list/')
+            if (len(Group.objects.filter(group=g).exclude(members=u)) == 0) and (sure == 1):
+                m = Membership.objects.get(user=request.user, group=g)
+                m.delete()
+                g.delete()
+                return HttpResponseRedirect('/groups/list/')
+            elif (len(Group.objects.filter(group=g).exclude(members=u)) > 0):    # if number of other users is > 0 then delete silentlu <<<< FIX HERE!!!
+                m = Membership.objects.get(user=request.user, group=g)
+                m.delete()
+                return HttpResponseRedirect('/groups/list/')
+            else:
+                return render_to_response('groups/quit_group_confirm.html', {'group_id': 'group_id'}, context_instance=RequestContext(request))
+#                return HttpResponseRedirect('/groups/quit/' + group_id + '/1/')
         except:
             return render_to_response('error.html',
                 {'title': 'error', 'message_col1': _("There is no such group or you are not a member of that group") + "."},
