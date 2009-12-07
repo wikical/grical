65260a3265e2885a0c6dd649496b3895f8959a15
miernik <miernik@users.noreply.github.com>
2009-12-07 03:12:22 +0100

    first version of feeds

--- events/models.py
+++ events/models.py
@@ -313,6 +313,8 @@ class Event(models.Model):
         return Tag.objects.get_for_object(self)
     def __unicode__(self):
 		return self.start.isoformat() + " : " + self.title
+    def get_absolute_url(self):
+        return 'http://dev.cloca.net/events/view_astext/' + str(self.id) + '/'
     class Meta:
         ordering = ['start']
         verbose_name = _('Event')
