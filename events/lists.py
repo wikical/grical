c8ffae6f4f17628f205aedb5502fb1c98adc0216
Miernik <miernik@users.noreply.github.com>
2010-03-03 06:13:52 +0100

    Now doing queries in chunks

--- events/lists.py
+++ events/lists.py
@@ -321,14 +321,14 @@ def list_up_to_max_events_ip_country_events(ip_addr, user_id, inital_exclude_eve
         # get 1 event at a time!
         try:
             if mode == 'continent':
-                e = Event.objects.filter(Q(start__gte=datetime.now()) & Q(country__in=other_countries_on_continent)).exclude(id__in=inital_exclude_event_id_list).order_by('start')[0:1].get()
+                e_list = Event.objects.filter(Q(start__gte=datetime.now()) & Q(country__in=other_countries_on_continent)).exclude(id__in=inital_exclude_event_id_list).order_by('start')[0:max_events]
             else:
-                e = Event.objects.filter(Q(start__gte=datetime.now()) & Q(country=country)).exclude(id__in=inital_exclude_event_id_list).order_by('start')[0:1].get()
-            print "found event %d" % e.id
-            inital_exclude_event_id_list.append(e.id)
-            if (is_event_viewable_by_user(e.id, user_id)):
-                list_of_events.append(e)
-                events_appended += 1
+                e_list = Event.objects.filter(Q(start__gte=datetime.now()) & Q(country=country)).exclude(id__in=inital_exclude_event_id_list).order_by('start')[0:max_events]
+            for e in e_list:
+                inital_exclude_event_id_list.append(e.id)
+                if (is_event_viewable_by_user(e.id, user_id)):
+                    list_of_events.append(e)
+                    events_appended += 1
         except Event.DoesNotExist:
             return list_of_events
 
