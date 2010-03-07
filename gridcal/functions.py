fc8af6501c182a99e70786ac0c5b79b459bc0d64
Miernik <mercurial@miernik.name>
2010-03-07 07:18:12 +0100

    fixed some bugs

--- gridcal/functions.py
+++ gridcal/functions.py
@@ -15,7 +15,7 @@ from django.contrib.sites.models import Site
 
 from tagging.models import Tag, TaggedItem
 
-from gridcalendar.gridcal.models import Event, EventUrl, EventTimechunk, EventDeadline, Filter, Group, COUNTRIES
+from gridcalendar.gridcal.models import Event, EventUrl, EventTimechunk, EventDeadline, Filter, Group, Membership, COUNTRIES
 from gridcalendar.gridcal.forms import SimplifiedEventForm, SimplifiedEventFormAnonymous, EventForm, EventFormAnonymous, FilterForm
 
 def getEventForm(user):
