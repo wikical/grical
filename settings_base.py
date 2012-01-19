66934797845030cb85df6b72713398420251e571
Antonis Christofides <antonis@djangodeployment.com>
2012-01-19 11:38:45 +0200

    settings overhaul

--- settings_base.py
+++ settings_base.py
@@ -3,6 +3,7 @@
 # vim: set expandtab tabstop=4 shiftwidth=4 textwidth=79 foldmethod=marker:
 #############################################################################
 # Copyright 2009-2011 Ivan Villanueva <ivan ät gridmind.org>
+# Copyright 2012      Antonis Christofides <anthony ät itia.ntua.gr>
 #
 # This file is part of GridCalendar.
 #
@@ -26,16 +27,7 @@
 import os
 import djcelery
 
-# See how settings_local.py should look like at the end of this file
-try:
-    from settings_local import * # pylint: disable-msg=W0401,W0614
-except ImportError:
-    pass # TODO: check that the whole thing is runable without settings_local
-
-try:
-    DEBUG
-except NameError:
-    DEBUG = False
+DEBUG = False
 
 # =============================================================================
 # specific GridCalendar settings {{{1
@@ -68,20 +60,16 @@ MAX_EVENTS_ON_ROOT_PAGE = 20
 # http://tools.ietf.org/html/rfc5545
 PRODID = '-//GridMind//NONSGML GridCalendar ' + VERSION + '//EN'
 
+REPLY_TO = None
+
 # =============================================================================
 # GeoIP, GEONAME and django-countries settings {{{1
 # =============================================================================
 # see # http://docs.djangoproject.com/en/dev/ref/contrib/gis/geoip/
 
 GEOIP_PATH = '/usr/share/GeoIP'
-try:
-    GEONAMES_URL
-except NameError:
-    GEONAMES_URL = 'http://api.geonames.org/'
-try:
-    GEONAMES_USERNAME
-except NameError:
-    GEONAMES_USERNAME = 'demo'
+GEONAMES_URL = 'http://api.geonames.org/'
+GEONAMES_USERNAME = 'demo'
 
 # =============================================================================
 # test settings {{{1
@@ -109,22 +97,10 @@ CSRF_FAILURE_VIEW = 'accounts.views.csrf_failure'
 # localization settings {{{1
 # =============================================================================
 
-try:
-    SHORT_DATETIME_FORMAT
-except NameError:
-    SHORT_DATETIME_FORMAT = 'Y-m-d H:i'
-try:
-    SHORT_DATE_FORMAT
-except NameError:
-    SHORT_DATE_FORMAT = 'Y-m-d'
-try:
-    TIME_FORMAT
-except NameError:
-    TIME_FORMAT = 'H:i'
-try:
-    DATE_FORMAT
-except NameError:
-    DATE_FORMAT = 'Y-m-d D'
+SHORT_DATETIME_FORMAT = 'Y-m-d H:i'
+SHORT_DATE_FORMAT = 'Y-m-d'
+TIME_FORMAT = 'H:i'
+DATE_FORMAT = 'Y-m-d D'
 
 # =============================================================================
 # for the tagging application {{{1
@@ -180,12 +156,6 @@ if DEBUG:
 # at the end additional middleware are conditionaly added
 MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
-)
-if DEBUG:
-    MIDDLEWARE_CLASSES += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware', )
-MIDDLEWARE_CLASSES += (
-    # see http://docs.djangoproject.com/en/1.2/ref/contrib/csrf/
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -200,45 +170,12 @@ MIDDLEWARE_CLASSES += (
     # other places) sometimes we create many revisions.
     # 'reversion.middleware.RevisionMiddleware',
  )
-if DEBUG:
-    MIDDLEWARE_CLASSES += (
-        'gridcalendar.middlewares.ProfileMiddleware', )
 
 # see http://docs.djangoproject.com/en/1.3/ref/contrib/messages/
 MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
 
 # https://docs.djangoproject.com/en/dev/topics/http/sessions/
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-
-# used by python-django-debug-toolbar
-if DEBUG:
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        # 'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel', # shows passwords !!!
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
-
-    # don't show the toolbar if the profiler
-    #   gridcalendar.middlewares.ProfileMiddleware
-    # is used, which happens when adding ?profile=1 to the request
-    def custom_show_toolbar(request):
-        if request.REQUEST.get('profile', False):
-            return False
-        return True
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-        #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
-        'HIDE_DJANGO_SQL': True,
-        #'TAG': 'div',
-    }
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
@@ -260,22 +197,12 @@ TEMPLATE_DIRS = (
     os.path.join( PROJECT_ROOT, "templates" ),
  )
 
-if DEBUG:
-    # List of callables that know how to import templates from various sources.
-    # without the cached loader:
-    TEMPLATE_LOADERS = (
-        'django.template.loaders.filesystem.load_template_source',
-        'django.template.loaders.app_directories.load_template_source',
-    )
-else:
-    # for the cached loader all tags must be thread-safe, see
-    # http://docs.djangoproject.com/en/dev/howto/custom-template-tags/#template-tag-thread-safety
-    TEMPLATE_LOADERS = (
-        ('django.template.loaders.cached.Loader', (
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader',
-        )),
-    )
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
 
 # =============================================================================
 # CACHE {{{1
@@ -286,6 +213,7 @@ CACHES = {
             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
             'LOCATION': '127.0.0.1:11211',
             'TIMEOUT': 300, # 5 minutes
+            'KEY_PREFIX': 'production',
         }, #TODO: use also the hot spare server
         'db': {
             'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
@@ -296,10 +224,6 @@ CACHES = {
             },
         },
 }
-if DEBUG:
-    CACHES['default']['KEY_PREFIX'] = 'debug'
-else:
-    CACHES['default']['KEY_PREFIX'] = 'production'
 
 # =============================================================================
 # celery
@@ -324,76 +248,18 @@ BROKER_VHOST = "/"
 # i18n and url settings {{{1
 # =============================================================================
 
-# see http://docs.djangoproject.com/en/1.2/ref/settings/#append-slash
 APPEND_SLASH = True
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-try:
-    TIME_ZONE
-except NameError:
-    TIME_ZONE = 'Europe/Berlin'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-try:
-    LANGUAGE_CODE
-except NameError:
-    LANGUAGE_CODE = 'en-us'
-
-try:
-    LANGUAGES
-except NameError:
-    LANGUAGES = ( ( 'en', 'English' ), )
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
+TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = 'en-us'
+LANGUAGES = ( ( 'en', 'English' ), )
 USE_I18N = True
-
-# TODO: use USE_L10N
-# see http://docs.djangoproject.com/en/dev/ref/settings/?from=olddocs#use-l10n
-# Notice also that when set to True, settings like TIME_FORMAT are not used
 USE_L10N = False
 
-# This is used by the django.contrib.sites, which is needed by for instance the
-# registration code we are using from a Debian package (upstream is:
-# http://bitbucket.org/ubernostrum/django-registration/wiki/Home )
-try:
-    SITE_ID
-except NameError:
-    SITE_ID = 1
-
-# Absolute path to the directory that holds media.
-try:
-    MEDIA_ROOT
-except NameError:
-    MEDIA_ROOT = os.path.join( PROJECT_ROOT, "media" )
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-try:
-    MEDIA_URL
-except NameError:
-    MEDIA_URL = '/m/'
-
-try:
-    ADMIN_MEDIA_PREFIX
-except NameError:
-    ADMIN_MEDIA_PREFIX = '/m/admin/'
-
-try:
-    ROOT_URLCONF
-except NameError:
-    ROOT_URLCONF = 'urls'
-
-try:
-    REPLY_TO
-except NameError:
-    REPLY_TO = None
+SITE_ID = 1
+MEDIA_ROOT = os.path.join( PROJECT_ROOT, "media" )
+MEDIA_URL = '/m/'
+ADMIN_MEDIA_PREFIX = '/m/admin/'
+ROOT_URLCONF = 'urls'
 
 # field names and synonyms/translations {{{1
 SYNONYMS = (
