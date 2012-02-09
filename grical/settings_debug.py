1b622a5d047c018c20c4529bf274a9764e1332e7
Ivan Villanueva <ivan@gridmind.org>
2012-02-09 12:19:20 +0100

    moved all to a subdirectory for better imports; disabled automated completion of geo data for non-new events; tiny hg script added

---
+++ grical/settings_debug.py
@@ -0,0 +1,44 @@
+from settings_base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+TEMPLATE_STRING_IF_INVALID = 'STRING_NOT_SET'
+MIDDLEWARE_CLASSES = (MIDDLEWARE_CLASSES[0], 
+                      'debug_toolbar.middleware.DebugToolbarMiddleware'
+                     ) + MIDDLEWARE_CLASSES[1:]
+MIDDLEWARE_CLASSES += (
+    'grical.middlewares.ProfileMiddleware', )
+INSTALLED_APPS = ('debug_toolbar',) + INSTALLED_APPS
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    # 'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel', # shows passwords !!!
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+# don't show the toolbar if the profiler
+#   grical.middlewares.ProfileMiddleware
+# is used, which happens when adding ?profile=1 to the request
+def custom_show_toolbar(request):
+    if request.REQUEST.get('profile', False):
+        return False
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+    #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
+    'HIDE_DJANGO_SQL': True,
+    #'TAG': 'div',
+}
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+CACHES['default']['KEY_PREFIX'] = 'debug'
+INTERNAL_IPS = ('127.0.0.1',)
--- wsgi/django.wsgi
+++ wsgi/django.wsgi
@@ -4,9 +4,7 @@ from sys import path
 
 path.append(dirname(realpath(dirname(__file__))))
 
-path.append(dirname(dirname(realpath(dirname(__file__)))))
-
-environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+environ['DJANGO_SETTINGS_MODULE'] = 'grical.settings'
 
 environ['CELERY_LOADER'] = 'django' # https://pypi.python.org/pypi/django-celery
 
@@ -14,8 +12,8 @@ import django.core.handlers.wsgi
 
 application = django.core.handlers.wsgi.WSGIHandler()
 
-import settings
+import grical.settings
 # see http://blog.dscpl.com.au/2008/12/using-modwsgi-when-developing-django.html
-if settings.DEBUG:
-    import gridcalendar.monitor
-    gridcalendar.monitor.start(interval=1.0)
+if grical.settings.DEBUG:
+    import grical.monitor
+    grical.monitor.start(interval=1.0)
