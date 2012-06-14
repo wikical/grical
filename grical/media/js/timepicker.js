97d8ce1b6cc5690d01d8827242602ceca59fd5bd
Ivan Villanueva <ivan@gridmind.org>
2012-06-14 13:48:11 +0200

    removed time picker javascript because it was changing times wrongly

--- grical/media/js/timepicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-    var options = {
-        timeFormat: 'H:i'
-    };
-    // http://jonthornton.github.com/jquery-timepicker/
-    $( ".timePicker" ).timepicker( options );
-});
