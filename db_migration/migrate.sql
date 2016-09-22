1b62d032062a28e4a16b2c128e21a52663bfce62
Stefanos Kozanis <stefanos@gridmind.org>
2016-09-22 15:46:28 +0300

    Working on the legacy db migration script, work in progress, missing content_type_id mappings, todo: finalize README

--- db_migration/migrate.sql
+++ db_migration/migrate.sql
@@ -46,12 +46,112 @@ INSERT INTO events_event (id, exact, timezone, version, coordinates, description
     SELECT id, exact, timezone, version, coordinates, description, address, city, country, tags, endtime, starttime, title, acronym, modification_time, creation_time, user_id FROM old_public.events_event;
 SELECT setval('events_event_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_event), 1), false);
 
+-- events_calendar
 
--- replaceit
+INSERT INTO events_calendar (id, date_added, group_id, event_id)
+    SELECT id, date_added, group_id, event_id FROM old_public.events_calendar;
+SELECT setval('events_calendar_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_calendar), 1), false);
 
-INSERT INTO replaceit (id, )
-    SELECT id,  FROM old_public.replaceit;
-SELECT setval('replaceit_id_seq', COALESCE((SELECT MAX(id)+1 FROM replaceit), 1), false);
+-- events_eventsession
 
+INSERT INTO events_eventsession (id, session_endtime, session_starttime, session_date, session_name, event_id)
+    SELECT id, session_endtime, session_starttime, session_date, session_name, event_id FROM old_public.events_eventsession;
+SELECT setval('events_eventsession_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_eventsession), 1), false);
+
+-- events_eventurl
+
+INSERT INTO events_eventurl (id, url, url_name, event_id)
+    SELECT id, url, url_name, event_id FROM old_public.events_eventurl;
+SELECT setval('events_eventurl_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_eventurl), 1), false);
+
+-- events_filter
+
+INSERT INTO events_filter (id, email, name, query, modification_time, user_id)
+    SELECT id, email, name, query, modification_time, user_id FROM old_public.events_filter;
+SELECT setval('events_filter_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_filter), 1), false);
+
+-- events_group
+
+INSERT INTO events_group (id, modification_time, creation_time, description, name)
+    SELECT id, modification_time, creation_time, description, name FROM old_public.events_group;
+SELECT setval('events_group_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_group), 1), false);
+
+-- events_groupinvitation
+
+INSERT INTO events_groupinvitation (id, issue_date, activation_key, as_administrator, group_id, guest_id, host_id)
+    SELECT id, issue_date, activation_key, as_administrator, group_id, guest_id, host_id FROM old_public.events_groupinvitation;
+SELECT setval('events_groupinvitation_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_groupinvitation), 1), false);
+
+-- events_membership
+
+INSERT INTO events_membership (id, date_joined, new_member_email, new_event_email, is_administrator, group_id, user_id)
+    SELECT id, date_joined, new_member_email, new_event_email, is_administrator, group_id, user_id FROM old_public.events_membership;
+SELECT setval('events_membership_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_membership), 1), false);
+
+-- events_recurrence
+-- NOTE: in old data, id was missing
+
+INSERT INTO events_recurrence (master_id, event_id)
+    SELECT master_id, event_id FROM old_public.events_recurrence;
+
+-- events_revisioninfo
+
+INSERT INTO events_revisioninfo (id, reason, redirect, as_text, revision_id)
+    SELECT id, reason, redirect, as_text, revision_id FROM old_public.events_revisioninfo;
+SELECT setval('events_revisioninfo_id_seq', COALESCE((SELECT MAX(id)+1 FROM events_revisioninfo), 1), false);
+
+-- oembed_providerrule
+-- NOTE: We don't migrate oembed_providerrule, it is already pre-populated by
+-- ./manage.py migrate. Just confirm data / ids are same.
+-- INSERT INTO oembed_providerrule (id, format, endpoint, regex, name)
+--    SELECT id, format, endpoint, regex, name FROM old_public.oembed_providerrule;
+-- SELECT setval('oembed_providerrule_id_seq', COALESCE((SELECT MAX(id)+1 FROM oembed_providerrule), 1), false);
+
+-- oembed_storedoembed
+
+INSERT INTO oembed_storedoembed (id, date_added, html, max_height, max_width, match)
+    SELECT id, date_added, html, max_height, max_width, match FROM old_public.oembed_storedoembed;
+SELECT setval('oembed_storedoembed_id_seq', COALESCE((SELECT MAX(id)+1 FROM oembed_storedoembed), 1), false);
+
+-- registration_registrationprofile
+
+-- NOTE: there is a new bool, not null, field called "activated". We assume
+-- and insert a "true" value.
+
+INSERT INTO registration_registrationprofile (id, activation_key, user_id, activated)
+    SELECT id, activation_key, user_id, true FROM old_public.registration_registrationprofile;
+SELECT setval('registration_registrationprofile_id_seq', COALESCE((SELECT MAX(id)+1 FROM registration_registrationprofile), 1), false);
+
+-- tagging_tag
+
+INSERT INTO tagging_tag (id, name)
+    SELECT id, name FROM old_public.tagging_tag;
+SELECT setval('tagging_tag_id_seq', COALESCE((SELECT MAX(id)+1 FROM tagging_tag), 1), false);
+
+-- tagging_taggeditem
+
+INSERT INTO tagging_taggeditem (id, object_id, content_type_id, tag_id)
+    SELECT id, object_id, content_type_id, tag_id FROM old_public.tagging_taggeditem;
+SELECT setval('tagging_taggeditem_id_seq', COALESCE((SELECT MAX(id)+1 FROM tagging_taggeditem), 1), false);
+
+-- reversion_revision
+-- NOTE: the former field "manager_slug" is removed. Value was 'default' in all rows
+-- it is assumed to be used as "db" value for reversion_version
+
+INSERT INTO reversion_revision (id, comment, user_id, date_created)
+    SELECT id, comment, user_id, date_created FROM old_public.reversion_revision;
+SELECT setval('reversion_revision_id_seq', COALESCE((SELECT MAX(id)+1 FROM reversion_revision), 1), false);
+
+-- reversion_version
+-- NOTE: former fields "object_id_int" and "type" are removed, new field
+-- "db" added assuming the value 'default'
+
+INSERT INTO reversion_version (id, object_repr, serialized_data, format, content_type_id, object_id, revision_id, db)
+    SELECT id, object_repr, serialized_data, format, content_type_id, object_id, revision_id, 'default' FROM old_public.reversion_version;
+SELECT setval('reversion_version_id_seq', COALESCE((SELECT MAX(id)+1 FROM reversion_version), 1), false);
+
+--
+-- TODO: FIXME: Map the content_type_id according to new (public) schema
+--
 
 ROLLBACK;
