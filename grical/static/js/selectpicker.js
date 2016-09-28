d00d559ae72e5555ef8f56c135a696074d620607
George Vlachos <george@gridmind.org>
2016-09-28 11:47:23 +0300

    improved event edit select field

---
+++ grical/static/js/selectpicker.js
@@ -0,0 +1,7 @@
+$(document).ready( function() {
+    var select = $( "select" );
+    select.addClass('selectpicker');
+    $('.selectpicker').selectpicker({
+        style: 'btn-outline-primary select-button'
+    });
+});
